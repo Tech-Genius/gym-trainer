d6f23685a3c904b4f0966387194f3488f710fbb2
Tech-Genius <adebimpehabeeb10@gmail.com>
2022-12-27 13:39:33 +0100

    hosting on render

--- thomasgym/settings.py
+++ thomasgym/settings.py
@@ -77,13 +77,18 @@ WSGI_APPLICATION = 'thomasgym.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'thomas-gym',
-        'USER': 'postgres',
-        'PASSWORD': 'wale2003',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'thomas-gym',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'wale2003',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432'
+    # }
+
+      'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
